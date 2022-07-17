f2738fb2c8144299bba96a2b13a82bc3eaaa188e
Harmaton <njagiiharmaton@gmail.com>
2022-07-17 12:27:01 +0300

    newbankapi

---
+++ src/main/kotlin/demo/HelloWorldController.kt
@@ -0,0 +1,16 @@
+package demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun helloWorld(): String{
+        return " Harmaton Njagi the spring master"
+    }
+}
---
+++ src/main/kotlin/demo/model/Bank.kt
@@ -0,0 +1,5 @@
+package demo.model
+
+data class Bank(val accountNumber: String, val trust: Double, val transactionFee: Int)
+
+
