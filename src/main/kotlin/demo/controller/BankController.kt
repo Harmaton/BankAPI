0dd1e6718e99ce10f8e9d6ac04ac3abb0c480e44
Harmaton <njagiiharmaton@gmail.com>
2022-07-25 16:43:01 +0300

    single bank mapping

--- src/main/kotlin/demo/controller/BankController.kt
+++ src/main/kotlin/demo/controller/BankController.kt
@@ -1,4 +1,15 @@
 package demo.controller
 
-class BankController {
+import demo.BankService.BankService
+import demo.model.Bank
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks() : Collection<Bank> = service.getBanks()
 }
