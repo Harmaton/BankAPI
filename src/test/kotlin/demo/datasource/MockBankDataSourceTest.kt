c1d22c3c4f6ba52ce168ba5ccc16bf0efad667d7
Harmaton <njagiiharmaton@gmail.com>
2022-11-03 16:35:42 +0300

    data source tests

--- src/test/kotlin/demo/datasource/MockBankDataSourceTest.kt
+++ src/test/kotlin/demo/datasource/MockBankDataSourceTest.kt
@@ -1,6 +1,7 @@
 package demo.datasource
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -16,10 +17,22 @@ internal class MockBankDataSourceTest {
 
         //when /act
         val banks = mockDataSource.getBanks()
-
         //then /assert
         Assertions.assertThat(banks).isNotEmpty
+        assertThat(banks.size).isGreaterThanOrEqualTo(3)
     }
+
+    @Test
+    fun `should provide some data` (){
+
+        //when
+        val banks = mockDataSource.getBanks()
+
+        //then
+        assertThat(banks).allMatch{it.accountNumber.isNotBlank()}
+        assertThat(banks).anyMatch{it.trust != 0.0}
+    }
+
 
 
 }
